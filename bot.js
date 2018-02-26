a01d7563d95b9001b07fcb0c0d8bb2e901dc9bc0
FHGDev <gideon.lingle14@gmail.com>
2018-02-26 14:18:16 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,7 @@ const bot = new discord.Client()
 const prefix = ".."
 
 bot.on('ready', () => {
-  bot.user.setActivity('my selfbot load...', {type: "WATCHING"})
-  setTimeout(() => {
-  bot.user.setActivity('Linkin Park', {type: "LISTENING"})
-  }, 10000)
+  bot.user.setActivity('ROBLOX', {type: "PLAYING"})
   console.log(`Selfbot Started at ${new Date().toString()}`)
 })
 
@@ -15,20 +12,25 @@ bot.on("message", message => {
   
   if (!message.author.id == 242734840829575169) return;
   
+  if (message.content == prefix + "setname") {
+    bot.user.setUsername(args[0])
+    message.delete(100)
+  }
+  
   if (message.content == prefix + "dnd") {
-    message.delete(1000)
+    message.delete(100)
     bot.user.setStatus(`dnd`)
   } 
   if (message.content == prefix + "online") {
-    message.delete(1000) 
+    message.delete(100) 
     bot.user.setStatus(`online`)
   }
   if (message.content == prefix + "idle") {
-    message.delete(1000)
+    message.delete(100)
     bot.user.setStatus(`idle`)
   }
   if (message.content == prefix + "inv") {
-    message.delete(1000)
+    message.delete(100)
     bot.user.setStatus(`invisible`)
   }
 })
